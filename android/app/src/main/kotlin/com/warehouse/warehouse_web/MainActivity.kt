bd8a72f1c780c4f72eea1c40f46d7b4075cadabe
wpghks <wpghks56@naver.com>
2024-04-11 11:35:33 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/warehouse/warehouse_web/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.warehouse.warehouse_web
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/src/const/http_link.dart
@@ -0,0 +1,1 @@
+const String HTTP_LINK = "http://115.178.65.180:8080/graphql";
