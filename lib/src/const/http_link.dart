b799da1b3b7bf8ae9972c63cad0b7610e46ea806
wpghks <wpghks56@naver.com>
2024-04-12 15:32:58 +0900

    [Chore] default header add

--- lib/src/const/http_link.dart
+++ lib/src/const/http_link.dart
@@ -7,8 +7,10 @@ final HttpLink httpLink = HttpLink(
   HTTP_LINK,
   defaultHeaders: {
     "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
+    "Access-Control-Allow-Headers":
+        "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
   },
-  
 );
 
 ValueNotifier<GraphQLClient> client = ValueNotifier(
