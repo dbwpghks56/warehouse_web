05c05fc722c3f1d045871868974b056fea9b43cf
wpghks <wpghks56@naver.com>
2024-04-12 16:23:25 +0900

    [Feature] netlify 설정 파일 다시

--- lib/src/const/http_link.dart
+++ lib/src/const/http_link.dart
@@ -1,7 +1,11 @@
+import 'dart:html' as html;
+
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
-const String HTTP_LINK = "http://115.178.65.180:8080/graphql";
+String HTTP_LINK = html.window.location.href.contains('localhost')
+    ? "http://115.178.65.180:8080/graphql"
+    : "api";
 
 final HttpLink httpLink = HttpLink(
   HTTP_LINK,
--- lib/src/view/base_view.dart
+++ lib/src/view/base_view.dart
@@ -1,3 +1,5 @@
+import 'dart:html' as html;
+
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
