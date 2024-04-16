8f1b8aba0c204815ff0d414c54b987c6b0889d0c
wpghks <wpghks56@naver.com>
2024-04-16 16:49:37 +0900

    [Feature] 내용이 modal 벗어나면 스크롤 발동

--- lib/src/view/question/widget/question_dialog.dart
+++ lib/src/view/question/widget/question_dialog.dart
@@ -21,47 +21,49 @@ class QuestionDialog extends StatelessWidget {
         padding: const EdgeInsets.all(16.0),
         width: 700,
         height: 600,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text(
-              "제목 : ${question.title}",
-              style: const TextStyle(
-                fontSize: 36,
-                fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                "제목 : ${question.title}",
+                style: const TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            Text(
-              "상세 : ${question.content}",
-              style: const TextStyle(
-                fontSize: 24,
+              Text(
+                "상세 : ${question.content}",
+                style: const TextStyle(
+                  fontSize: 24,
+                ),
               ),
-            ),
-            Text("출처 : ${question.source!}"),
-            Text(
-              "난이도 : ${getRank(question)}",
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
+              Text("출처 : ${question.source!}"),
+              Text(
+                "난이도 : ${getRank(question)}",
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            Text("시간 제한 : ${question.timeLimit ?? '정보 없음'}"),
-            Text("공간 제한 : ${question.memoryLimit ?? '정보 없음'}"),
-            Text("평균시도 : ${question.averageTries ?? '정보 없음'}"),
-            Text("총 시도 : ${question.totalTries ?? '정보 없음'}"),
-            Text(
-              "도전한 사람 : ${question.totalPerson ?? '정보 없음'}",
-            ),
-            Text(
-              "정답률 : ${question.successRate ?? '정보없음'}",
-            ),
-            Text(
-              "완료한 사람 : ${question.totalSuccess ?? '정보 없음'}",
-            ),
-            Row(
-              children: tags.map((e) => QuestionTag(tagName: e)).toList(),
-            ),
-          ],
+              Text("시간 제한 : ${question.timeLimit ?? '정보 없음'}"),
+              Text("공간 제한 : ${question.memoryLimit ?? '정보 없음'}"),
+              Text("평균시도 : ${question.averageTries ?? '정보 없음'}"),
+              Text("총 시도 : ${question.totalTries ?? '정보 없음'}"),
+              Text(
+                "도전한 사람 : ${question.totalPerson ?? '정보 없음'}",
+              ),
+              Text(
+                "정답률 : ${question.successRate ?? '정보없음'}",
+              ),
+              Text(
+                "완료한 사람 : ${question.totalSuccess ?? '정보 없음'}",
+              ),
+              Row(
+                children: tags.map((e) => QuestionTag(tagName: e)).toList(),
+              ),
+            ],
+          ),
         ),
       ),
     );
